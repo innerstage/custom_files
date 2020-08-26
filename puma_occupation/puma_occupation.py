4b2ee5a7e7a34a28d192d7e2454be78505ee8792
innerstage <mbarbierif@gmail.com>
2020-08-26 01:17:21 -0400

    Try Except implementation

--- puma_occupation/puma_occupation.py
+++ puma_occupation/puma_occupation.py
@@ -8,15 +8,9 @@ import csv
 pumas_df = pd.read_csv("pumas.csv")
 pumas_name_map = {k:v for (k,v) in zip(pumas_df["geoid"], pumas_df["name"])}
 
-# Query for each PUMA
-N = len(list(pumas_name_map.keys()))
-a = 0
-
-for puma in list(pumas_name_map.keys()):
-    a += 1
+# Define a query function
+def make_query(puma):
     query = "https://keyite-api.datausa.io/cubes/pums_1/aggregate?drilldown%5B%5D=%5BPUMS+Occupation%5D.%5BDetailed+Occupation%5D&cut%5B%5D=%5BGeography%5D.%5BGeography%5D.%5BPUMA%5D.%26%5B{}%5D&measures%5B%5D=Total+Population&measures%5B%5D=Average+Wage&nonempty=true&distinct=false&parents=false&debug=true&sparse=true".format(puma)
-
-    print("{:,}/{:,} ({:.2f}%) Processing {}".format(a, N, a/N*100, puma))
     r = requests.get(query)
     json_r = json.loads(r.text)
 
@@ -33,3 +27,21 @@ for puma in list(pumas_name_map.keys()):
         )
 
     df.to_csv("final_file.csv", index=False, mode="a", quoting=csv.QUOTE_NONNUMERIC)
+
+    return 0
+
+
+# Query for each PUMA
+N = len(list(pumas_name_map.keys()))
+a = 0
+
+for puma in list(pumas_name_map.keys()):
+    a += 1
+    print("{:,}/{:,} ({:.2f}%) Processing {}".format(a, N, a/N*100, puma))
+    try:
+        make_query(puma)
+    except:
+        with open("problematic_pumas.log","a") as file:
+            file.write("{}\n".format(puma))
+        continue
+    
